c7fd8df348af6e18cff9d28223be0db431c8a779
sebastian karlsson <sebbekarlsson97@gmail.com>
2025-10-12 03:21:02 +0200

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # express-dir
 > Directory-based routing for express
+
+
+## Usage
+```typescript
+import express from "express";
+import { setup } from "../../src/setup";
+import pathlib from "path";
+
+const app = express();
+const port = 3000;
+
+setup(app, {
+  routesDirectory: pathlib.resolve(pathlib.join(__dirname, "./routes")),
+}).then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
+});
+
+```
--- example/src/index.ts
+++ example/src/index.ts
@@ -1,15 +1,14 @@
-import express from 'express';
-import { setup } from '../../src/setup';
-import pathlib from 'path';
+import express from "express";
+import { setup } from "../../src/setup";
+import pathlib from "path";
 
 const app = express();
 const port = 3000;
 
-
 setup(app, {
-  routesDirectory: pathlib.resolve(pathlib.join(__dirname, './routes')) 
-}).catch(e => console.error(e));
-
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-})
+  routesDirectory: pathlib.resolve(pathlib.join(__dirname, "./routes")),
+}).then(() => {
+  app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+  });
+});
